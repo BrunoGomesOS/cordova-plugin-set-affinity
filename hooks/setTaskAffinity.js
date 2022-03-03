ccf1de9fb79b057c0d295af583d981403d88c032
Bruno Gomes <65655967+BrunoGomesOS@users.noreply.github.com>
2022-03-03 10:00:32 +0000

    Update setTaskAffinity.js

--- hooks/setTaskAffinity.js
+++ hooks/setTaskAffinity.js
@@ -54,7 +54,7 @@ var updateTaskAffinity = (function () {
         if (root) {
             var applicationElement = root.find("./application");
             if (applicationElement) {
-                var sEmpty = null;
+                var sEmpty = '';
                 root.set("xmlns:tools", "http://schemas.android.com/tools");
                 applicationElement.set("android:taskAffinity", sEmpty);
                 applicationElement.set("tools:replace", "android:taskAffinity");
